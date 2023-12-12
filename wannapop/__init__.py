46861a99902b81a6e9430ef13c3481f80e9da7b2
oscargdzs <ogomezd@fp.insjoaquimmir.cat>
2023-12-12 16:12:08 +0100

    0.2 Debug toolbar

--- wannapop/__init__.py
+++ wannapop/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 import os
 from config import Config 
@@ -17,7 +18,7 @@ def create_app():
     app.config.from_object(Config)
     # ruta absoluta d'aquesta carpeta
     basedir = os.path.abspath(os.path.dirname(__file__)) 
-
+    app.debug = True
     # paràmetre que farà servir SQLAlchemy per a connectar-se
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + basedir + "/../database.db"
     # mostre als logs les ordres SQL que s'executen
@@ -27,6 +28,7 @@ def create_app():
     login_manager.init_app(app)
     db_manager.init_app(app)
     principal_manager.init_app(app)
+    toolbar = DebugToolbarExtension(app)
     
     with app.app_context():
         from . import routes_main, routes_auth, routes_admin
@@ -35,7 +37,6 @@ def create_app():
         app.register_blueprint(routes_main.main_bp)
         app.register_blueprint(routes_auth.auth_bp)
         app.register_blueprint(routes_admin.admin_bp)
-        
 
     app.logger.info("Aplicació iniciada")
 
